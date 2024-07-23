2fcdcc54379bf3a3939ebe8348612c9c86eeb8fd
Max Batrov <maxbatrov@gmail.com>
2024-07-23 21:09:23 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Расчет бонусных миль.");
+        int priceTicket = 24876;
+        int bonus = priceTicket / 20;
+        System.out.println("Ваших бонусных миль:" + bonus);
+    }
+}
